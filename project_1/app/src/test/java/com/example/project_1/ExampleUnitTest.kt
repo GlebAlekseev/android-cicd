fa1ef2ed66a4249ae544e7cc7682d856d84416c1
GlebAlekseev <glebalekseev@gmail.com>
2022-05-01 00:34:57 +0300

    Default utests

--- project_1/app/src/test/java/com/example/project_1/ExampleUnitTest.kt
+++ project_1/app/src/test/java/com/example/project_1/ExampleUnitTest.kt
@@ -14,8 +14,8 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-    @Test
-    fun addition2_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
+//    @Test
+//    fun addition2_isCorrect() {
+//        assertEquals(4, 2 + 2)
+//    }
 }
